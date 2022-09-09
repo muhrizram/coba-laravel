e46de3ed58852e398258fbf210f4d4506f4e3e06
muhrizram <muhrizram19@gmail.com>
2022-09-09 13:22:11 +0700

    Menambahkan gambar dan data pada halaman About

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
     <title>Muhrizram Blog | About</title>
 </head>
 <body>
     <h1>Halaman About</h1>
+    <h3>Muhammad Rizki Ramadhan</h3>
+    <p>muhrizram19@gmail.com</p>
+    <img src="img/muhrizram.jpg" alt="Muhammad Rizki Ramadhan" width="200">
 </body>
 </html>
