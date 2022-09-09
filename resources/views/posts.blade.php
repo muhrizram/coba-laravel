302ad9748ebb61f4aa7e252965039131e02513db
muhrizram <muhrizram19@gmail.com>
2022-09-10 05:58:42 +0700

    Membuat gambar menjadi bundar

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.main')
 
 @section('isi')
+    <h1>Semua Post</h1>
     @foreach ($posts as $post)
-        <article class="mb-5">
+        <article class="mt-4">
             <h3><a href="/posts/{{ $post['slug'] }}">{{ $post['title'] }}</a></h3>
             <h4>By: {{ $post['author'] }}</h4>
             <p>{{ $post['body'] }}</p>
