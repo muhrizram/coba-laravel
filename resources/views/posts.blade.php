e185bb8bac666b9414e58dc1b900485718198ddf
muhrizram <muhrizram19@gmail.com>
2022-09-09 19:30:07 +0700

    Mengubah file about.blade.php, mengubah file home.blade.php, menambahkan file main.blade.php di folder layouts, mengubah file posts.blade.php

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Muhrizram Blog | Posts</title>
-</head>
-<body>
+@extends('layouts.main')
+
+@section('isi')
     <h1>Halaman Posts</h1>
-</body>
-</html>
+@endsection
