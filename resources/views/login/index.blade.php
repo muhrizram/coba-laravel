c12c6e5c0fc0cdaf4923fac3f9d475a6ab4ae433
muhrizram <muhrizram19@gmail.com>
2022-09-17 07:38:35 +0700

    Menambahkan fitur login (Auth attempt)

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -3,25 +3,29 @@
 @section('isi')
 <div class="row justify-content-center">
     <div class="col-md-4">
-
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            @if(session()->has('success'))
+        @if(session()->has('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
                 {{ session('success') }}
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            @endif
-          </div>
-
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>  
+            </div>
+        @endif
 
         <main class="form-signin w-100 m-auto">
             <h1 class="h3 mb-3 fw-normal text-center">Please Login</h1>
-            <form>
+            <form action="/login" method="post">
+                @csrf
                 <div class="form-floating">
-                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                    <label for="floatingInput">Email address</label>
+                    <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" value="{{ old('email') }}" autofocus required>
+                    @error('email')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                    <label for="email">Email address</label>
                 </div>
                 <div class="form-floating">
-                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                    <label for="floatingPassword">Password</label>
+                    <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
+                    <label for="password">Password</label>
                 </div>
                 <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
             </form>
--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,8 @@ Route::get('/categories', [CategoriesController::class, 'index']);
 
 Route::get('/login', [LoginController::class, 'index']);
 
+Route::post('/login', [LoginController::class, 'authenticate']);
+
 Route::get('/register', [RegisterController::class, 'index']);
 
 Route::post('/register', [RegisterController::class, 'store']);
