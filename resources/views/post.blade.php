aecb866838cb34133474f0b354da21f5b5296dd5
muhrizram <muhrizram19@gmail.com>
2022-09-25 11:29:30 +0700

    Menambahkan Update & Delete Post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center mb-5">
             <div class="col-md-8">
                 <h2 class="mb-3">{{ $post->title }}</h2>
-                <p>By: <a href="/blog?authors={{ $post->author->username }}"
+                <p>By: <a href="/blog?author={{ $post->author->username }}"
                         class="text-decoration-none">{{ $post->author->name }}</a> in <a
                         href="/blog?category={{ $post->category->slug }}"
                         class="text-decoration-none">{{ $post->category->name }}</a></p>
